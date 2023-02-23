d81cdc350a3e1fa5e6e1b41d98f44486a4ee78b1
xiaomihu69 <2559221382@qq.com>
2023-02-23 17:10:27 +0800

    小程序的首页

---
+++ 经验分享小程序/data/index-list.js
@@ -0,0 +1,27 @@
+
+var typeList = [{
+	ArticleTypeID : 0,
+	ArticleTypeName : "全"
+},{
+	ArticleTypeID : 12,
+	ArticleTypeName : "每日分享"
+},{
+	ArticleTypeID : 875,
+	ArticleTypeName : "操作指南"
+},{
+	ArticleTypeID : 2038,
+	ArticleTypeName : "常见问题"
+},{
+	ArticleTypeID : 2033,
+	ArticleTypeName : "奇闻趣事"
+},{
+	ArticleTypeID : 1,
+	ArticleTypeName : "更新进度"
+}]
+
+var articleList= [{}]
+
+
+module.exports.articles=articleList;
+module.exports.typeList=typeList;
+
