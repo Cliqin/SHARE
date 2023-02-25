2cbb439324d8db185b90860bee287f60c23c9571
1077602679 <105963181+1077602679@users.noreply.github.com>
2023-02-25 21:38:14 +0800

    我的主页mine
    加载了获取用户信息按钮
    对 mine.js,mine.wxml进行了增加

--- SHARE/pages/mine/mine.js
+++ SHARE/pages/mine/mine.js
@@ -64,3 +64,27 @@ Page({
 
   }
 })
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },})
