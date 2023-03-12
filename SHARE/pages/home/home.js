33900189d3f64d60fd3a9ad1b87f775b00758f0a
Cliqin <1113764188@qq.com>
2023-03-12 12:46:59 +0800

    Revert "Merge branch 'main' of https://github.com/Cliqin/SHARE"
    This reverts commit 5c1ae2188d24d38ef55147435624e4d061d5cffe, reversing
    changes made to 5f503c6915a9fd18b8f7f40a219741468cb99306.

--- SHARE/pages/home/home.js
+++ SHARE/pages/home/home.js
@@ -1,67 +1,45 @@
-data: {
-
-const app = getApp()
-
+var app =getApp()
 Page({
   data: {
-    userInfo: {},
-imgUrls: [
-'/images/lunbo3.png',
-],
-list: ['🎉“计算机学院创新小分队', '🎉SHARE上线', '敬请期待'],
-indicatorDots: true,
-autoplay: true,
-interval: 5000,
-interval2: 11000,
-duration: 1000,
-duration2: 2000,
-windowHeight: wx.getSystemInfoSync().windowHeight,
-windowWidth: wx.getSystemInfoSync().windowWidth,
-btnSize: 0,
-token: wx.getStorageSync('token'),
-status: 2,
-canIUseGetUserProfile: false,
-hasUserInfo: false,
-},
-onLoad() {
-console.log('999' + this.data.token);
-var that = this;
-this.setData({
-btnSize: 0.9 * 0.32 * this.data.windowHeight,
-})
-const token = wx.getStorageSync('token')
-if (token == '') {
-wx.navigateTo({
-url: '../login/login',
-})
-}
-if (wx.getUserProfile) {
-this.setData({
-canIUseGetUserProfile: true
-})
-}
-},
-
-getUserProfile(e) {
-// 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
-// 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-wx.getUserProfile({
-desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-success: (res) => {
-console.log(e)
-console.log(res)
-this.setData({
-userInfo: res.userInfo,
-hasUserInfo: true
-})
-app.globalData.userInfo=res.userInfo
-console.log(app.globalData.userInfo,'44')
-}
-})
-},
-onShow() {
-
-
+    imgUrls: [
+      '/images/lunbo3.png',
+    ],
+    list: ['🎉“计算机学院创新小分队', '🎉SHARE上线', '敬请期待'],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    interval2: 11000,
+    duration: 1000,
+    duration2: 2000,
+    windowHeight: wx.getSystemInfoSync().windowHeight,
+    windowWidth: wx.getSystemInfoSync().windowWidth,
+    btnSize: 0,
+    token: wx.getStorageSync('token'),
+    status: 2,
+    canIUseGetUserProfile: false,
+    hasUserInfo: false,
+  },
+  onLoad() {
+    console.log('999' + this.data.token);
+    var that = this;
+    this.setData({
+      btnSize: 0.8 * 0.32 * this.data.windowHeight,
+    })
+    const token = wx.getStorageSync('token')
+    if (token == '') {
+      wx.navigateTo({
+        url: '../login/login',
+      })
+    }
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  onShow() {
+    
+    
   },
   onHide: function () {
     this.setData({
@@ -100,9 +78,9 @@ onShow() {
   },
   toGroup() {
     wx.navigateTo({
-      url: '/pages/index/index',
-    })
-
+        url: '/pages/index/index',
+      })
+    
   },
   toPark() {
     wx.navigateTo({
@@ -138,7 +116,7 @@ onShow() {
 
         app.globalData.userInfo = res.userInfo
         wx.setStorageSync('userInfo', res.userInfo)//键值对
-    
+
         wx.navigateBack({
           success(res){
             wx.showToast({
--- SHARE/pages/mine/mine.js
+++ SHARE/pages/mine/mine.js
@@ -1,26 +1,16 @@
-
-const app = getApp()
-
 Page({
   data: {
-    userInfo:app.globalData.userInfo,
+    userInfo: {},
     hasUserInfo: false,
     canIUseGetUserProfile: false,
-    test:"123"
   },
   onLoad() {
     if (wx.getUserProfile) {
       this.setData({
-        userInfo: app.globalData.userInfo,  
+        canIUseGetUserProfile: true
       })
     }
   },
-  onReady(){
-    // this.setData({
-    //   userInfo: app.globalData.userInfo,   
-    // })
-  },
-
   getUserProfile(e) {
     wx.getUserProfile({
       desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
@@ -31,5 +21,4 @@ Page({
         })
       }
     })
-  },
-})
+  },})
